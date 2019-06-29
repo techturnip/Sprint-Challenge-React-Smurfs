5ed36d8a59b89180432162089f5d50403c6764af
Tyler Turnipseed <tyler@techturnip.us>
2019-06-29 12:50:59 -0400

    added cdn link to material-icons to create edit/delete icons in Smurf.js component with materialize-css, added the icons and some styling to display them on the right side of the Smurf cards

--- village/src/components/Smurf.js
+++ village/src/components/Smurf.js
@@ -4,9 +4,15 @@ const Smurf = props => {
   return (
     <div className="Smurf card hoverable">
       <div className="card-content">
-        <span className="card-title">{props.name}</span>
-        <strong>{props.height} tall</strong>
-        <p>{props.age} smurf years old</p>
+        <div>
+          <span className="card-title">{props.name}</span>
+          <strong>{props.height} tall</strong>
+          <p>{props.age} smurf years old</p>
+        </div>
+        <div>
+          <i className="small blue-text material-icons">edit</i>
+          <i className="small red-text text-lighten-1 material-icons">delete</i>
+        </div>
       </div>
     </div>
   );
