b3393cddd8ffd869f9906dd0658b067ea1b44aee
Tyler Turnipseed <tyler@techturnip.us>
2019-06-29 12:06:24 -0400

    imported materialize into App.js and initialized the JS functionality in the componentDidMout() method, added classes from materialize to the JSX template of App.js, imported axios into App.js and used .get to grab the array of smurf objects from the /smurfs endpoint from the server and set the array into state from the componentDidMount() method

--- village/src/App.js
+++ village/src/App.js
@@ -1,15 +1,29 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import axios from "axios";
 
-import './App.css';
-import SmurfForm from './components/SmurfForm';
-import Smurfs from './components/Smurfs';
+import "materialize-css/dist/css/materialize.min.css";
+import M from "materialize-css";
+import "./App.css";
+import SmurfForm from "./components/SmurfForm";
+import Smurfs from "./components/Smurfs";
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      smurfs: [],
+      smurfs: []
     };
+  }
+
+  componentDidMount() {
+    // Audo initialize Materialize
+    M.AutoInit();
+
+    // Grab Smurfs from the server and set to state
+    axios
+      .get("http://localhost:3333/smurfs")
+      .then(res => this.setState({ smurfs: res.data }))
+      .catch(err => console.log(err));
   }
   // add any needed code to ensure that the smurfs collection exists on state and it has data coming from the server
   // Notice what your map function is looping over and returning inside of Smurfs.
@@ -17,8 +31,29 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <SmurfForm />
-        <Smurfs smurfs={this.state.smurfs} />
+        <nav className="container blue">
+          <div className="nav-wrapper">
+            <a href="#" className="brand-logo">
+              Logo
+            </a>
+            <ul id="nav-mobile" className="right hide-on-med-and-down">
+              <li>
+                <a href="sass.html">Sass</a>
+              </li>
+              <li>
+                <a href="badges.html">Components</a>
+              </li>
+              <li>
+                <a href="collapsible.html">JavaScript</a>
+              </li>
+            </ul>
+          </div>
+        </nav>
+
+        <div className="container">
+          <SmurfForm />
+          <Smurfs smurfs={this.state.smurfs} />
+        </div>
       </div>
     );
   }
